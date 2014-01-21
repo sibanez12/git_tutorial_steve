56cce5bff9de87d7eba835b855d885209370e9c2
sibanez12 <sibanez@g.hmc.edu>
2014-01-21 09:01:38 -0800

    Modified impulse_response.m to * instead of / and plotted output

--- impulse_response.m
+++ impulse_response.m
@@ -8,8 +8,10 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = X.*Y;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+
+plot(t,h)
 
 end
 
